0260fd557ecd105e2d3aa919efa1fa0cd9866793
jyhan03 <ihan@fit.vut.cz>
2025-03-21 16:51:40 +0100

    add example

---
+++ example/run_example.py
@@ -0,0 +1,23 @@
+from diarizen.pipelines.inference import DiariZenPipeline
+
+# load pre-trained model
+diar_pipeline = DiariZenPipeline.from_pretrained("BUT-FIT/diarizen-meeting-base")
+
+# apply diarization pipeline
+diar_results = diar_pipeline('EN2002a.Array1-01.wav')
+
+# print results
+for turn, _, speaker in diar_results.itertracks(yield_label=True):
+    print(f"start={turn.start:.1f}s stop={turn.end:.1f}s speaker_{speaker}")
+# start=0.0s stop=1.7s speaker_0
+# start=0.0s stop=0.7s speaker_1
+# start=0.8s stop=8.0s speaker_4
+# ...
+
+# load pre-trained model and save RTTM result
+diar_pipeline = DiariZenPipeline.from_pretrained(
+        "BUT-FIT/diarizen-meeting-base",
+        rttm_out_dir='.'
+)
+# apply diarization pipeline
+diar_results = diar_pipeline('EN2002a.Array1-01.wav', sess_name='EN2002a')
